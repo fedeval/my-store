36906dc484a01d8bfdcf239e718c5b2609aad143
fedeval <federico.valenza5@gmail.com>
2021-06-11 16:35:23 +0300

    feat: generate product-item component, child of product-list + styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductItemComponent } from './components/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
